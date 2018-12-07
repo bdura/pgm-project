9ffbb0925e34a632d0ff08ab93a64d192927a310
Basile Dura <basile.dura@polytechnique.edu>
2018-12-07 14:47:38 -0500

    Sinai2

--- autoencoders/proteins.py
+++ autoencoders/proteins.py
@@ -194,6 +194,126 @@ class WAESinai(WassersteinAutoEncoder):
         return x.view(n, 24 * 82)
 
 
+class WAESinai2(nn.Module):
+
+    def __init__(self, ksi=10., hidden_dimension=2, loss_function=F.binary_cross_entropy):
+        super(WAESinai2, self).__init__()
+
+        self.ksi = ksi
+        self.hidden_dimension = hidden_dimension
+
+        self.loss_function = loss_function
+
+        self.fc1 = nn.Linear(24 * 82, 250)
+        self.fc2 = nn.Linear(250, 250)
+        self.fc3 = nn.Linear(250, 250)
+        self.fc4 = nn.Linear(250, hidden_dimension)
+
+        self.bn1 = nn.BatchNorm1d(250)
+
+        self.fc5 = nn.Linear(hidden_dimension, 250)
+        self.fc6 = nn.Linear(250, 250)
+        self.fc7 = nn.Linear(250, 250)
+        self.fc8 = nn.Linear(250, 24 * 82)
+
+    def encode(self, x):
+
+        n = x.size(0)
+        x = x.view(n, -1)
+
+        x = F.elu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.elu(self.fc2(x))
+        x = self.bn1(x)
+        x = F.elu(self.fc3(x))
+        x = self.fc4(x)
+
+        return x
+
+    def decode(self, x):
+
+        n = x.size(0)
+
+        x = F.elu(self.fc5(x))
+        x = F.elu(self.fc6(x))
+        x = self.dropout(x)
+        x = F.elu(self.fc7(x))
+        x = self.fc8(x)
+
+        x = x.view(n, 24, 82)
+
+        # We push the pixels towards 0 and 1
+        x = F.softmax(x, dim=1)
+
+        return x.view(n, 24 * 82)
+
+    def log_softmax(self, x):
+
+        x = self.encode(x)
+
+        n = x.size(0)
+
+        x = F.relu(self.fc5(x))
+        x = F.relu(self.fc6(x))
+        x = F.relu(self.fc7(x))
+        x = self.fc8(x)
+
+        x = x.view(n, 24, 82)
+
+        # We push the pixels towards 0 and 1
+        x = F.log_softmax(x, dim=1)
+
+        return x.view(n, 24 * 82)
+
+    def forward(self, x):
+        z = self.encode(x)
+        x_tilde = self.decode(z)
+
+        return x_tilde, z
+
+    def loss(self, x, x_tilde, z, device):
+
+        n = x.size(0)
+
+        recon_loss = self.loss_function(x_tilde.view(n, -1), x.view(n, -1))
+
+        z_fake = torch.randn(n, self.hidden_dimension).to(device)
+
+        kernel_zf_zf = self.kernel(z_fake, z_fake)
+        kernel_z_z = self.kernel(z, z)
+        kernel_z_zf = self.kernel(z, z_fake)
+
+        mmd_loss = ((1 - torch.eye(n).to(device)) * (kernel_zf_zf + kernel_z_z)).sum() \
+                   / (n * (n-1)) - 2 * kernel_z_zf.mean()
+
+        total_loss = recon_loss + self.ksi * mmd_loss
+
+        return total_loss
+
+    def kernel(self, x, y):
+        """
+        Returns a matrix K where :math:`K_{i, j} = k(x_i, y_j)
+
+        Args:
+            x: a PyTorch Tensor
+            y: a PyTorch Tensor
+
+        Returns:
+            The kernel computed for every pair of x and y.
+        """
+
+        assert x.size() == y.size()
+
+        # We use the advised constant, with sigma=1
+        # c is the expected square distance between 2 vectors sampled from Pz
+        c = self.hidden_dimension * 2
+
+        x_ = x.unsqueeze(0)
+        y_ = y.unsqueeze(1)
+
+        return c / (c + (x_ - y_).pow(2).sum(2))
+
+
 def test(epoch, model, test_loader, device, writer):
     model.eval()
     test_loss = 0
